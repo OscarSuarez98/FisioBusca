8ee539930f4e4944ba280c57162de2d3c14ec0de
OscarSuarez98 <oscarmsm98@outlook.com>
2023-02-13 17:38:58 -0600

    Proyecto Completo

---
+++ README.md
@@ -0,0 +1,2 @@
+# ecommerce-de-fisioterapia
+se desarrollará una pagina web con el fin de ofrecer servicios de fisioterapia, conectando fisioterapeutas con clientes 
---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Masaje",
+    precio: 50,
+    img:
+      "https://flordelizspa.com/wp-content/uploads/2016/03/Masaje-terapeutico-scaled.jpeg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Acondicionamiento Fisico",
+    precio: 100,
+    img:
+      "https://www.tqma.com.ec/images/articulos/bienestar/2019/08/preparacion_y_acondicionamiento/foto_principal.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Terapia Fisica",
+    precio: 150,
+    img:
+      "https://doralfamilyjournal.com/wp-content/uploads/2022/01/therapy-800x445.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Valoracion Fisica",
+    precio: 200,
+    img:
+      "https://sifo.comillas.edu/pluginfile.php/2772231/course/overviewfiles/rawcoverimage.jpg",
+    cantidad: 1,
+  },
+];
