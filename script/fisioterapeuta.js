2bf96a8da34bff105710901badd72c8e1290ecc5
OscarSuarez98 <oscarmsm98@outlook.com>
2023-02-17 22:51:17 -0600

    Actualizacion json

---
+++ script/fisioterapeuta.js
@@ -0,0 +1,15 @@
+
+let nombreusuario= prompt("Introduce tu Nombre");
+const edadUsuario = parseInt(prompt ("Introduce tu Edad"));
+if(edadUsuario > 18){ 
+    alert ("Puedes Registrarte")
+} else{
+    alert("Aun no puedes resgistrarte")
+}
+console.log=(nombreusuario)
+console.log=(edadUsuario)
+
+const saludo = alert(`Hola ${nombreusuario}, tienes ${edadUsuario} años de edad`)
+
+let usuario = true
+nombreusuario ? (console.log("Puedes registrarte")) : (console.log("No puedes registrarte"))
